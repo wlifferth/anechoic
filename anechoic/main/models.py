9d6d126193a513a186cc8db1f6c2c9aead7d4cfc
Will Lifferth <wlifferth@icloud.com>
2018-04-07 17:42:05 -0400

    Added Position and Argument models

--- anechoic/main/models.py
+++ anechoic/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+     statement = models.CharField(max_length=100, label="Stated Position")
+     description = models.TextField(max_length=1000, label="Description of position")
+     link = models.URLField(label="More information")
+
+     def __str__(self):
+         return "Position: \"{}\"".format(self.statement)
+
+class Argument(models.Model):
+    link = models.URLField(label="Link to Argument")
+    title = models.CharField(max_length=100, label="Title of argument")
+    description = models.TextField(max_length=1000, label="Description of argument")
+    rating = models.FloatField(label="Rating of argument")
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Argument: \"{}\"".format(self.title)
