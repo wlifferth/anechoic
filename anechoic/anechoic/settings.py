faf78deebc09354a78df66b243480106a295e805
Will Lifferth <wlifferth@icloud.com>
2018-04-07 17:42:25 -0400

    Added main as an 'installed app'

--- anechoic/anechoic/settings.py
+++ anechoic/anechoic/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['anechoic.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
