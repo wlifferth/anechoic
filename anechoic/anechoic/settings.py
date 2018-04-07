79687c484b5af9f084e32fa1dd04523556fd1786
Will Lifferth <wlifferth@icloud.com>
2018-04-07 16:54:31 -0400

    Fixed STATIC_ROOT

--- anechoic/anechoic/settings.py
+++ anechoic/anechoic/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
+STATIC_ROOT= os.path.join(PROJECT_ROOT,'staticfiles/')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT,'static/'),
 )
