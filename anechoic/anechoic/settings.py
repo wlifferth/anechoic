e856c22c81da2fda0c0d461ed9cd41fc92d8e7be
Will Lifferth <wlifferth@icloud.com>
2018-04-07 20:51:24 -0400

    Created a login page; closes #9

--- anechoic/anechoic/settings.py
+++ anechoic/anechoic/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'anechoic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,5 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static/'),
 )
+
+LOGIN_REDIRECT_URL = '/'
