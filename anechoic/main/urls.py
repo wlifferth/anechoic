ee6fed312836b917767b646ecc612f8478fa1d76
Will Lifferth <wlifferth@icloud.com>
2018-04-07 20:16:30 -0400

    Created the basic intake form; closes #4

--- anechoic/main/urls.py
+++ anechoic/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('u/<str:username>/getPositions/', views.getPositions, name='getPositions'),
     path('register/', views.register, name='register'),
     path('hello/', views.hello, name='hello'),
     ]
--- anechoic/main/views.py
+++ anechoic/main/views.py
@@ -4,11 +4,27 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Position, UserPosition
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def hello(request):
     return HttpResponse("Hello!")
+
+def getPositions(request, username):
+    context = {}
+    if request.method == 'POST':
+        positions = Position.objects.all()
+        user = User.objects.filter(username=username).get();
+        for position in positions:
+            if 'p' + str(position.id) in request.POST:
+                newUserPosition = UserPosition(user=user, position=position, rating=int(request.POST['p' + str(position.id)]))
+                newUserPosition.save()
+        return HttpResponse("You successfully submitted the form!")
+    else:
+        context['positionQuestions'] = Position.objects.all()
+        return render(request, 'main/positionSurvey.html', context=context)
 
 def register(request):
     if request.method == 'POST':
